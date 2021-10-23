659616169b0188264b176854d592180c9947821f
origin <wjdalsckd777@naver.com>
2021-10-23 14:58:25 +0900

    modified

--- web/my-app/src/app/App.js
+++ web/my-app/src/app/App.js
@@ -1,10 +1,11 @@
 // import './App.scss';
 import './App.css';
 
-import ComputeMain from  '../compute/ComputeMain.js';
-import Card from '../card/Card';
+import ComputeCor from '../compute/ComputeCor.js';
+import ComputeIndi from '../compute/ComputeIndi.js';
 
-import {BrowserRouter as Router, Route, Switch, useHistory} from "react-router-dom";
+// import ComputeMain from  '../compute/ComputeMain.js';
+
 import React , {useState, useEffect} from 'react';
 import axios from 'axios'
 
@@ -15,34 +16,37 @@ import step4 from "../img/step4.png"
 
 
 function App() {
-<head>
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400&display=swap" rel="stylesheet">
-</head>
-
   // flask 연결 예시
-  const [getMessage, setGetMessage] = useState([]);
+  // const [getMessage, setGetMessage] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://localhost:5000/flask/hello').then(response => {
-      console.log("SUCCESS", response)
-      setGetMessage(response)
-    }).catch(error => {
-      console.log(error)
-    })
-  }, [])
-  
-  let history = useHistory();
+  // useEffect(() => {
+  //   axios.get('http://localhost:5000/flask/hello').then(response => {
+  //     console.log("SUCCESS", response)
+  //     setGetMessage(response)
+  //   }).catch(error => {
+  //     console.log(error)
+  //   })
+  // }, [])
+
+  const [btnCor, setBtnCor] = useState(false);
+  const [btnIndi, setBtnIndi] = useState(false);
+
+  const ChangeColorCor = () => {
+    document.getElementById("cor").style.backgroundColor="#3182f6"
+    document.getElementById("cor").style.color="white"
+
+    document.getElementById("indi").style.backgroundColor="white"
+    document.getElementById("indi").style.color="black"
+  }
+
+  const ChangeColorIndi = () => {
+    document.getElementById("indi").style.backgroundColor="#3182f6"
+    document.getElementById("indi").style.color="white"
     
-  function handleCorClick() {
-    history.push("/cor");
-  }
+    document.getElementById("cor").style.backgroundColor="white"
+    document.getElementById("cor").style.color="black"
 
-  function handleIndiClick() {
-      history.push("/indi");
   }
-
   return (
     <div className="App"> 
       <h1>e-풀필먼트 배송비 계산기</h1>
@@ -52,32 +56,53 @@ function App() {
         <img src= {step3} alt="" />
         <img src= {step4} alt="" />
       </div>
-      <div className="body">
+      {/* <div className="body">
         <div>
-          <button onClick={handleCorClick}>기업</button>
+          <button onClick={() => setBtnCor(true)}>기업</button>
           <span> 입고비와 보관비를 확인할 수 있습니다.</span>
         </div>
         <div>
-          <button onClick={handleIndiClick}>개인</button>
+          <button onClick={() => setBtnIndi(true)}>개인</button>
           <span> 배송 방법을 선택할 수 있습니다.</span>
         </div>
+      </div> */}
+      <div className="container">
+        <div className="container-header">
+          
+          <button id="cor" onClick={() => {
+            setBtnCor(true)
+            setBtnIndi(false)
+            ChangeColorCor()
+          }}>기업
+          </button>
+          
+          <button id="indi" onClick={() => {
+            setBtnIndi(true)
+            setBtnCor(false)
+            ChangeColorIndi()
+          }}>개인
+          </button>
+        
+        </div>
+        <div className="container-body">
+          {
+            btnCor ? (
+              <div>
+                <span style={{ borderTop:"2opx"}}>입고비와 보관비를 확인할 수 있습니다.</span>
+                <ComputeCor />
+              </div>
+            ) : btnIndi ?(
+              <div>
+                <span style={{ borderTop:"2opx"}}>배송방법을 선택 할 수 있습니다.</span>
+                <ComputeIndi />
+              </div>
+            ) : (
+              null
+            )
+          }
+        </div>
       </div>
-      <Router >
-        {/* <Route 
-          path="/cor" 
-          render = {(props) => 
-            <ComputeMain 
-              {...props} />}
-        /> */}
-        <Route 
-          path="/indi" 
-          render = {(props) => 
-            <ComputeMain 
-              {...props} />}
-            />
-      </Router>
-      
-      
+    
     </div>
   );
 }
---
+++ web/my-app/src/compute/ComputeCor.js
@@ -0,0 +1,14 @@
+import "./ComputeCor.css"
+import React, {useState, useEffect} from 'react';
+
+
+function ComputeCor() {
+
+  return (
+    <div className="ComputeCor">
+      
+    </div>
+  );
+}
+
+export default ComputeCor;
