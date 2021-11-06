dcd60215a8c903e9098f84ddb9c8f3d05bc4e7bc
origin <wjdalsckd777@naver.com>
2021-11-06 10:36:27 +0900

    modified

--- web/my-app/src/compute/ComputeIndi.js
+++ web/my-app/src/compute/ComputeIndi.js
@@ -235,7 +235,7 @@ function ComputeIndi() {
         최적의 배송비 리스트 확인하기
       </button>
       {
-        resultInfo["2_0"] ? (
+        btnSum ? (
           <Table result={resultInfo} />
         ) : null
       }
